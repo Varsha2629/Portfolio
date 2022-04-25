06cd8b5cd2865567b940de6cff414e751dea475e
Varsha2629 <varsha3051@gmail.com>
2022-04-24 23:51:42 -0400

    change header and footer

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -5,10 +5,11 @@ const Header = () => {
     <div className="main"> 
        <nav className="menu">
           <a href="http://localhost:3000/" className="logo"><img src="/img/logo192.jpg" alt="logo" /></a>
-            <li><a href="#about" > About</a></li>
-            <li><a href="#skills" > Skills</a></li>            
-            <li><a href="#projects" > Projects</a></li>        
-            <li><a href="#contect" > Contact</a></li>
+            <li><a href="#about"> Home</a></li>
+            <li><a href="#about"> About</a></li>
+            <li><a href="#skills"> Skills</a></li>            
+            <li><a href="#projects"> Projects</a></li>        
+            <li><a href="#contect"> Contact</a></li>
        </nav>     
 
     
