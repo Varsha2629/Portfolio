6ab10141194a8cf0dde347764ab7d17a1da4f4c4
Varsha2629 <varsha3051@gmail.com>
2022-06-08 23:39:54 -0400

    reChange content in project

--- src/components/Section/Projects/Project.jsx
+++ src/components/Section/Projects/Project.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Project = ({ project }) => {
     // console.log(props)
     return (
-        <div className="project-deatils">
-              {project.img}
+        <div className="project-deatils">        
+             {project.img}
              <h3>{project.title}</h3>
         </div>
     )
