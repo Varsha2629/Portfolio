1fe4be5ed3361a60566dab6499336ade0f0ad5cf
Varsha2629 <varsha3051@gmail.com>
2022-08-04 16:19:46 -0400

    remove test and Varsha file

--- Varsha.jsx
+++
@@ -1,9 +0,0 @@
-const AboutMe = () => {
-  return (
-    <div>
-        <h1>Welcome to Varsha Panchal's Page</h1>
-        <h3>I am full stack developer. Looking for junior full-stack developer / Web developer oportunity.</h3>        
-        <h4>Feel free to connect with me</h4>
-    </div>
-  )
-}
