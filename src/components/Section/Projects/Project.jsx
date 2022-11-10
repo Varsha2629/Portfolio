3efba079a519baced65ee6c952066e62a49b5e75
Varsha2629 <varsha3051@gmail.com>
2022-11-10 11:38:07 -0500

    nothing to add

--- src/components/Section/Projects/Project.jsx
+++ src/components/Section/Projects/Project.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 
 const Project = ({ project }) => {
-    
-    return (
-                
-                       <div class="project-card">
-                          <div class="img-div">
-                             <img variant="top" src={project.img} alt="card-img" />
-                          </div>
-                          <div className="card-body">
-                               <div class="project-card-header">
-                                <h3>{project.title}</h3>
-                               </div>
-                            
-                               <div class="overlay">
-                                <div class="project-card-text">Build microservices using a modern back-end framework. Learn how to develop custom APIs and to communicate with databases to persist data.</div>
-                                <h5>NodeJs, Express, CSS, Git</h5>
-                               </div>
-                                
-                           </div>  
-                       </div>
-    )
-}
+  return (
+    <div class="project-card">
+      <div class="img-div">
+        <img variant="top" src={project.img} alt="card-img" />
+      </div>
+      <div className="card-body">
+        <div class="project-card-header">
+          <h3>{project.title}</h3>
+        </div>
+
+        <div class="overlay">
+          <div class="project-card-text">
+            Build microservices using a modern back-end framework. Learn how to
+            develop custom APIs and to communicate with databases to persist
+            data.
+          </div>
+          <h5>NodeJs, Express, CSS, Git</h5>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Project;
