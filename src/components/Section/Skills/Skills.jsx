5e615148e1aea0ddec7fa4e8ad0306d02d941414
Varsha2629 <varsha3051@gmail.com>
2022-11-14 11:57:55 -0500

    work on content and style

--- src/components/Section/Skills/Skills.jsx
+++ src/components/Section/Skills/Skills.jsx
@@ -6,53 +6,52 @@ const Skills = () => {
     <div id="skills">
       <h1>Skills</h1>
       <p>What I do</p>
-      <div className="card">
+        <div className="card">
 
-        <div className="skill-img">
-          <img src="img/svg/JavaScript.svg" alt="javascript" />
-          <p>JavaScript</p>
-        </div>            
-        <div className="skill-img">  
-          <img src="img/svg/React.svg" alt="react" />
-          <p>React</p>
-        </div>
-        <div className="skill-img">  
-          <img src="img/svg/HTML5.svg" alt="html" />
-          <p>HTML5</p>
-        </div>
-        <div className="skill-img"> 
-          <img src="img/svg/CSS3.svg" alt="css" />
-          <p>CSS3</p>
-        </div>
-        <div className="skill-img"> 
-          <img src="img/svg/Github.svg" alt="github" />
-          <p>GitHub</p>
-        </div>
-        <div className="skill-img"> 
-          <img src="img/svg/NodeExpress.svg" alt="node" />
-          <p>NodeJs</p>
-        </div>
+          <div className="skill-img">
+            <img src="img/svg/JavaScript.svg" alt="javascript" />
+            <p>JavaScript</p>
+          </div>            
+          <div className="skill-img">  
+            <img src="img/svg/React.svg" alt="react" />
+            <p>React</p>
+          </div>
+          <div className="skill-img">  
+            <img src="img/svg/HTML5.svg" alt="html" />
+            <p>HTML5</p>
+          </div>
+          <div className="skill-img"> 
+            <img src="img/svg/CSS3.svg" alt="css" />
+            <p>CSS3</p>
+          </div>
+          <div className="skill-img"> 
+            <img src="img/svg/Github.svg" alt="github" />
+            <p>GitHub</p>
+          </div>
+          <div className="skill-img"> 
+            <img src="img/svg/NodeExpress.svg" alt="node" />
+            <p>NodeJs</p>
+          </div>
 
+          
+          <div className="skill-img">  
+            <img src="img/svg/HTML5.svg" alt="html" />
+            <p>HTML5</p>
+          </div>
+          <div className="skill-img"> 
+            <img src="img/svg/CSS3.svg" alt="css" />
+            <p>CSS3</p>
+          </div>
+          <div className="skill-img"> 
+            <img src="img/svg/Github.svg" alt="github" />
+            <p>GitHub</p>
+          </div>
+          <div className="skill-img"> 
+            <img src="img/svg/NodeExpress.svg" alt="node" />
+            <p>NodeJs</p>
+          </div>
         
-        <div className="skill-img">  
-          <img src="img/svg/HTML5.svg" alt="html" />
-          <p>HTML5</p>
         </div>
-        <div className="skill-img"> 
-          <img src="img/svg/CSS3.svg" alt="css" />
-          <p>CSS3</p>
-        </div>
-        <div className="skill-img"> 
-          <img src="img/svg/Github.svg" alt="github" />
-          <p>GitHub</p>
-        </div>
-        <div className="skill-img"> 
-          <img src="img/svg/NodeExpress.svg" alt="node" />
-          <p>NodeJs</p>
-        </div>
-
-      
-    </div>
 
       <div className="service">
         <h1>Work Experience</h1>
@@ -67,46 +66,46 @@ const Skills = () => {
           <div className="service_list">
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Develop an intuitive user interface for the users.</p>
+                <p className="description">Develop an intuitive user interface for the users.</p>
               </div>
             
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Work on the integration of the demand chain system.</p>
+                <p className="description">Work on the integration of the demand chain system.</p>
               </div>
           
 
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Use of HTML5, jQuery, and CSS for the development of Views/Front End for the legacy MVC application.</p>
+                <p className="description">Use of HTML5, jQuery, and CSS for the development of Views/Front End for the legacy MVC application.</p>
               </div>
           
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Apply Server-side validations in MVC Models using Data Annotations.</p>
+                <p className="description">Apply Server-side validations in MVC Models using Data Annotations.</p>
               </div>
           
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p> Perform code reviews of fellow developers.</p>
+                <p className="description"> Perform code reviews of fellow developers.</p>
               </div>
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Used Git for version control.</p>
+                <p className="description">Used Git for version control.</p>
               </div>
           
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Upgrade and develop new Stored Procedures as per the requirement</p>
+                <p className="description">Upgrade and develop new Stored Procedures as per the requirement</p>
               </div>
            
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Enhance the system modules and functionality.</p>
+                <p className="description">Enhance the system modules and functionality.</p>
               </div>
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Consumed the API and worked on ReactJS-based UI.</p>
+                <p className="description">Consumed the API and worked on ReactJS-based UI.</p>
               </div>
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Collaborated with QA on deployment to different environments, and bug fixes.</p>
+                <p className="description">Collaborated with QA on deployment to different environments, and bug fixes.</p>
               </div>
           
           </div>
@@ -115,59 +114,45 @@ const Skills = () => {
 
           <div className="service-container">
           <div className="service__head">
-            <h3>Shreesoftic</h3>
-            <small class="services__item-desc">FullStack Developer</small>
+            <h3>Creative Werk Designs</h3>
+            <small class="services__item-desc">FrontEnd Developer</small>
           </div>
           <div className="service_list">
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Develop an intuitive user interface for the users.</p>
+                <p className="description">Created, maintain, and enhanced the websites to make them more attractive and user-friendly according to the
+customer’s needs using front-end and back-end web technologies</p>
               </div>
             
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Work on the integration of the demand chain system.</p>
+                <p className="description">Worked with different kinds of e-commerce, business, media, entertainment, and portfolio websites, and gave
+special attention to making them responsive and cross-browser compatible.</p>
               </div>
-          
-
-            
+                      
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Use of HTML5, jQuery, and CSS for the development of Views/Front End for the legacy MVC application.</p>
-              </div>
-          
-              <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Apply Server-side validations in MVC Models using Data Annotations.</p>
+                <p className="description">Maintained an organized workflow using a version control system tool like GitHub.</p>
               </div>
           
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p> Perform code reviews of fellow developers.</p>
-              </div>
-            
-              <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Used Git for version control.</p>
+                <p className="description">Performed CMS Services like PSD/HTML to WordPress and WordPress theme customization.</p>
               </div>
           
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Upgrade and develop new Stored Procedures as per the requirement</p>
-              </div>
-           
-              <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Enhance the system modules and functionality.</p>
+                <p className="description"> Communicate technical issues in an understandable manner to the team and clients</p>
               </div>
             
               <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Consumed the API and worked on ReactJS-based UI.</p>
+                <p className="description">Demonstrated the ability to work diligently under pressure to meet deadlines</p>
               </div>
-            
-              <div className="service__list-icon"><img src="img/svg/checkarrow.svg" alt="checkarrow" />
-                <p>Collaborated with QA on deployment to different environments, and bug fixes.</p>
-              </div>
+          
+             
           
           </div>
           
           </div>       
 
-       </div>
+        </div>
       </div>
     </div>
   );
