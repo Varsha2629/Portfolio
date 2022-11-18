14995835bbe255c5a44714a192aefae1b7e6c20c
Varsha2629 <varsha3051@gmail.com>
2022-11-17 21:38:19 -0500

    working on about section

--- src/components/Section/About/About.jsx
+++ src/components/Section/About/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
         <div id="about">
             <h1>Who I am</h1>  
             <div className="about-test">
-                <p> I am a detail-oriented Front-End developer with a passion for creating, repairing, building, and innovating. My diverse background in corporate and investing allows me to excel at logical thinking and creative problem-solving, allowing development to be the perfect field for my skill set. I love dry, responsive, accessible code and bring experience developing applications in Javascript, React, jQuery, CSS/SCSS, and HTML.
+                <p> I am a detail-oriented Front-End developer with a passion for creating, building, and innovating. My diverse background in computer science and investing allows me to excel at logical thinking and creative problem-solving, allowing development to be the perfect field for my skill set. I love dry, responsive, accessible code and bring experience developing applications in Javascript, React, jQuery, CSS/SCSS, and HTML.
                 While attending Lighthouse labs I had the opportunity to create multiple projects using various languages. The projects that I worked on while at LHL had many up and downs but the final products turned out to be something I am proud of and well worth it.</p>         
                 
                 <div className="profile-image"> 
